5fb9797446b841e5bc06d410488fd8775a3fe291
Shayan Alam <ishayanalam@gmail.com>
2025-04-28 10:48:08 +0600

    fixed comments

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -14,7 +14,7 @@ const admin = require("../middlewares/isAdmin.js");
 
 router.post("/signup", signUpValidation, userController.signup);
 router.post("/login", loginValidation, userController.login);
-//router.get("/admin/counselings", auth.isAuthorized, admin.isAdmin, userController.getAllCounselingRequests);
+
 
 //token er jonno
 router.get("/get-user", auth.isAuthorized, userController.getUser);
